1af8efff26264b0c3b685fa26475a74934faab69
Nicolas <nicolas.aguilar180193@gmail.com>
2023-12-14 18:00:41 -0300

    Table with People data

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,58 @@
 import React from "react";
+import { DataGrid, GridRenderCellParams } from '@mui/x-data-grid';
+import { People } from "@/data";
 
-export interface HomeInterface {}
+export interface HomeInterface { }
 
 const Home: React.FC<HomeInterface> = () => {
+	const pageSize = 5
+	const pageSizeOptions = [5, 10, 25]
+	const columns = [
+		{
+			field: 'name',
+			headerName: 'Name',
+			flex: 1,
+			minWidth: 150,
+			renderCell: (params: GridRenderCellParams) => <>{params.value}</>
+		},
+		{
+			field: 'category',
+			headerName: 'Category',
+			flex: 1,
+			minWidth: 150,
+			renderCell: (params: GridRenderCellParams) => <>{params.value}</>
+
+		},
+		{
+			field: 'company',
+			headerName: 'Company',
+			flex: 1,
+			minWidth: 150,
+			renderCell: (params: GridRenderCellParams) => <>{params.value}</>
+		},
+		{
+			field: 'levelOfHappiness',
+			headerName: 'Happiness',
+			flex: 1,
+			minWidth: 150,
+			renderCell: (params: GridRenderCellParams) => <>{params.value}</>
+		}
+	]
 
 	return (
-		<div>
-			Home
-		</div>
+		<DataGrid
+			disableColumnSelector
+			disableRowSelectionOnClick
+			disableDensitySelector
+			autoHeight
+			initialState={{
+				pagination: { paginationModel: { pageSize: pageSize } },
+			}}
+			pageSizeOptions={pageSizeOptions}
+			rows={People}
+			columns={columns}
+			getRowId={(row: any) => row.id}
+		/>
 	)
 }
 
