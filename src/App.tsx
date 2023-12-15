ed143da6595c6ac12f03d7cf4e875b4f73660def
Nicolas <nicolas.aguilar180193@gmail.com>
2023-12-15 00:37:35 -0300

    Layout styled component and little css changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import './App.css'
 import { Navbar } from './components'
 import { Home } from './pages'
+import { LayoutContainer } from './styled-components'
 
 function App() {
 
   return (
     <>
       <Navbar/>
+      <LayoutContainer>
       <Home/>
+      </LayoutContainer>
     </>
   )
 }
