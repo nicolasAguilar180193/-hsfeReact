96e45a8ab0a3f7a9408a4e11fff5c02a656def1b
Nicolas <nicolas.aguilar180193@gmail.com>
2024-02-08 00:29:16 -0300

    Select People with checkbox

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,36 @@
 import React from "react";
 import { DataGrid, GridRenderCellParams } from '@mui/x-data-grid';
 import { People } from "@/data";
+import { Person } from "@/models";
+import { Checkbox } from "@mui/material";
 
 export interface HomeInterface { }
 
 const Home: React.FC<HomeInterface> = () => {
+	const [selectedPeople, setSelectedPeople] = React.useState<Person[]>([])
 	const pageSize = 5
 	const pageSizeOptions = [5, 10, 25]
+
+	const findPerson = (person: Person) => !!selectedPeople.find(p => p.id === person.id)
+	const filterPerson = (person: Person) => selectedPeople.filter(p => p.id !== person.id)
+
+	const handleChange = (person: Person) => {
+		setSelectedPeople(findPerson(person) ? 
+			filterPerson(person) : [...selectedPeople, person]
+		)
+	}
+
 	const columns = [
+		{
+			field: 'actions',
+			type: 'actions',
+			sortable: false,
+			headerName: '',
+			width: 50,
+			renderCell: (params: GridRenderCellParams) => (
+				<>{<Checkbox size="small" checked={findPerson(params.row)} onChange={() => handleChange(params.row)}/>}</>
+			)
+		},
 		{
 			field: 'name',
 			headerName: 'Name',
