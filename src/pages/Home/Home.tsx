2f1711b80b843fda32ae230ac092c630fbc1325a
Nicolas <nicolas.aguilar180193@gmail.com>
2024-02-11 15:30:41 -0300

    Refactor Home, create people table component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,90 +1,23 @@
 import React, { useEffect } from "react";
-import { DataGrid, GridRenderCellParams } from '@mui/x-data-grid';
-import { People } from "@/data";
-import { Person } from "@/models";
-import { Checkbox } from "@mui/material";
+import { PeopleTable } from "./components";
+import { addPeople } from "@/redux/states";
 import { useDispatch } from "react-redux";
-import { addFavorite, addPeople } from "@/redux/states";
-import store from "@/redux/store";
+import { People } from "@/data";
+
 
 export interface HomeInterface { }
 
 const Home: React.FC<HomeInterface> = () => {
-	const [selectedPeople, setSelectedPeople] = React.useState<Person[]>([])
 	const dispatch = useDispatch()
-	const pageSize = 5
-	const pageSizeOptions = [5, 10, 25]
-
-	const findPerson = (person: Person) => !!selectedPeople.find(p => p.id === person.id)
-	const filterPerson = (person: Person) => selectedPeople.filter(p => p.id !== person.id)
-
-	const handleChange = (person: Person) => {
-		const filteredPeople = findPerson(person) ? filterPerson(person) : [...selectedPeople, person]
-		dispatch(addFavorite(filteredPeople))
-		setSelectedPeople(filteredPeople)
-	}
-
-	const columns = [
-		{
-			field: 'actions',
-			type: 'actions',
-			sortable: false,
-			headerName: '',
-			width: 50,
-			renderCell: (params: GridRenderCellParams) => (
-				<>{<Checkbox size="small" checked={findPerson(params.row)} onChange={() => handleChange(params.row)}/>}</>
-			)
-		},
-		{
-			field: 'name',
-			headerName: 'Name',
-			flex: 1,
-			minWidth: 150,
-			renderCell: (params: GridRenderCellParams) => <>{params.value}</>
-		},
-		{
-			field: 'category',
-			headerName: 'Category',
-			flex: 1,
-			minWidth: 150,
-			renderCell: (params: GridRenderCellParams) => <>{params.value}</>
-
-		},
-		{
-			field: 'company',
-			headerName: 'Company',
-			flex: 1,
-			minWidth: 150,
-			renderCell: (params: GridRenderCellParams) => <>{params.value}</>
-		},
-		{
-			field: 'levelOfHappiness',
-			headerName: 'Happiness',
-			flex: 1,
-			minWidth: 150,
-			renderCell: (params: GridRenderCellParams) => <>{params.value}</>
-		}
-	]
 
 	useEffect(() => {
 		dispatch(addPeople(People))
 	}, [])
 
 	return (
-		<DataGrid
-			disableColumnSelector
-			disableRowSelectionOnClick
-			disableDensitySelector
-			autoHeight
-			initialState={{
-				pagination: { paginationModel: { pageSize: pageSize } },
-			}}
-			pageSizeOptions={pageSizeOptions}
-			rows={store.getState().people}
-			columns={columns}
-			getRowId={(row: any) => row.id}
-		/>
+		<PeopleTable />
 	)
 }
 
 export default Home;
+
---
+++ src/pages/Home/components/PeopleTable/PeopleTable.tsx
@@ -0,0 +1,87 @@
+import React from "react";
+import { Person } from "@/models";
+import { addFavorite } from "@/redux/states";
+import { AppStore } from "@/redux/store";
+import { Checkbox } from "@mui/material";
+import { DataGrid, GridRenderCellParams } from "@mui/x-data-grid";
+import { useDispatch, useSelector } from "react-redux";
+
+export interface PeopleTableInterface { }
+
+const PeopleTable: React.FC<PeopleTableInterface> = () => {
+	const [selectedPeople, setSelectedPeople] = React.useState<Person[]>([])
+	const dispatch = useDispatch()
+	const pageSize = 5
+	const pageSizeOptions = [5, 10, 25]
+	const statePeople = useSelector((store: AppStore) => store.people)
+
+	const findPerson = (person: Person) => !!selectedPeople.find(p => p.id === person.id)
+	const filterPerson = (person: Person) => selectedPeople.filter(p => p.id !== person.id)
+
+	const handleChange = (person: Person) => {
+		const filteredPeople = findPerson(person) ? filterPerson(person) : [...selectedPeople, person]
+		dispatch(addFavorite(filteredPeople))
+		setSelectedPeople(filteredPeople)
+	}
+
+	const columns = [
+		{
+			field: 'actions',
+			type: 'actions',
+			sortable: false,
+			headerName: '',
+			width: 50,
+			renderCell: (params: GridRenderCellParams) => (
+				<>{<Checkbox size="small" checked={findPerson(params.row)} onChange={() => handleChange(params.row)} />}</>
+			)
+		},
+		{
+			field: 'name',
+			headerName: 'Name',
+			flex: 1,
+			minWidth: 150,
+			renderCell: (params: GridRenderCellParams) => <>{params.value}</>
+		},
+		{
+			field: 'category',
+			headerName: 'Category',
+			flex: 1,
+			minWidth: 150,
+			renderCell: (params: GridRenderCellParams) => <>{params.value}</>
+
+		},
+		{
+			field: 'company',
+			headerName: 'Company',
+			flex: 1,
+			minWidth: 150,
+			renderCell: (params: GridRenderCellParams) => <>{params.value}</>
+		},
+		{
+			field: 'levelOfHappiness',
+			headerName: 'Happiness',
+			flex: 1,
+			minWidth: 150,
+			renderCell: (params: GridRenderCellParams) => <>{params.value}</>
+		}
+	]
+
+	return (
+		<DataGrid
+			disableColumnSelector
+			disableRowSelectionOnClick
+			disableDensitySelector
+			autoHeight
+			initialState={{
+				pagination: { paginationModel: { pageSize: pageSize } },
+			}}
+			pageSizeOptions={pageSizeOptions}
+			rows={statePeople}
+			columns={columns}
+			getRowId={(row: any) => row.id}
+		/>
+	)
+
+}
+
+export default PeopleTable;
