de357a7e12f58473916b4a5538a14e99a75ee4b9
Nicolas <nicolas.aguilar180193@gmail.com>
2024-02-11 15:37:50 -0300

    Bug, now get favorites from storage

--- src/pages/Home/components/PeopleTable/PeopleTable.tsx
+++ src/pages/Home/components/PeopleTable/PeopleTable.tsx
@@ -14,9 +14,10 @@ const PeopleTable: React.FC<PeopleTableInterface> = () => {
 	const pageSize = 5
 	const pageSizeOptions = [5, 10, 25]
 	const statePeople = useSelector((store: AppStore) => store.people)
+	const favoritePeople = useSelector((store: AppStore) => store.favorites);
 
-	const findPerson = (person: Person) => !!selectedPeople.find(p => p.id === person.id)
-	const filterPerson = (person: Person) => selectedPeople.filter(p => p.id !== person.id)
+	const findPerson = (person: Person) => !!favoritePeople.find(p => p.id === person.id)
+	const filterPerson = (person: Person) => favoritePeople.filter(p => p.id !== person.id)
 
 	const handleChange = (person: Person) => {
 		const filteredPeople = findPerson(person) ? filterPerson(person) : [...selectedPeople, person]
